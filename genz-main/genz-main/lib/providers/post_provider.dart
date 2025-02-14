7e49bf06fee81b90b7907e1cbb0bb6d597408ed8
AmirrChaieb <amirrchaieb@gmail.com>
2025-02-14 01:56:36 +0100

    first commit

---
+++ genz-main/genz-main/lib/providers/post_provider.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:yoser/models/post.dart';
+
+import '../api/api_service.dart';
+import '../models/pagination.dart';
+
+
+final postsProvider = FutureProvider.family<List<Post>?, PaginationModel>(
+        (ref, paginationModel){
+      final apiRepository = ref.watch(apiService);
+
+      return apiRepository.getPosts(paginationModel.page, paginationModel.pageSize);
+
+    }
+);
